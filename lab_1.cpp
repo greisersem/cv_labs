82a1071eb0cb3956e7ec5146de9a35acf4f78c8a
greisersem <grejser.se@edu.spbstu.ru>
2025-12-01 12:51:27 +0000

    Lab_1

--- lab_1.cpp
+++ lab_1.cpp
@@ -12,7 +12,7 @@ int main()
     int width = background.cols;
 
     int A = heigth / 4;
-    double w = CV_PI * 8;
+    double w = 2 * CV_PI / width;
 
     int robot_w = width / 10;
     int robot_h = heigth / 10;
@@ -23,6 +23,14 @@ int main()
         int y = (heigth / 2) + (A * std::sin(x * w));
         cv::circle(background, cv::Point(x, y), 5, line_color, -1);
         cv::imshow("Sinus", background);
+        cv::waitKey(1);
+    }
+
+    for (int x = 0; x < width; x++) {
+        int y = (heigth / 2) + (A * std::sin(x * w));
+        cv::circle(background, cv::Point(x, y), 5, line_color, -1);
+        cv::imshow("Sinus", background);
+        cv::waitKey(1);
     }
 
     cv::waitKey();
