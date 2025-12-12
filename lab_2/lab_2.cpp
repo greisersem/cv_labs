c57b0f25122da51b7b6bd8dcf451ad3247b2f111
greisersem <grejser.se@edu.spbstu.ru>
2025-12-12 08:28:10 +0000

    Lab_2 update

--- lab_2/lab_2.cpp
+++ lab_2/lab_2.cpp
@@ -2,9 +2,11 @@
 #include <opencv2/highgui.hpp>
 #include <opencv2/core.hpp>
 #include <opencv2/imgcodecs.hpp>
+#include <opencv2/intensity_transform.hpp>
+#include <iostream>
 
 
-cv::Mat calc_diff(cv::Mat img_1, cv::Mat img_2)
+void calc_diff(cv::Mat img_1, cv::Mat img_2)
 {
     cv::Mat diff_mat;
 
@@ -14,27 +16,32 @@ cv::Mat calc_diff(cv::Mat img_1, cv::Mat img_2)
 
     int non_zero_px = cv::countNonZero(diff_mat);
 
-    return ((1.0 - (double)non_zero_px / (diff_mat.cols() * diff_mat.rows())) * 100.0)
+    double s = (1.0 - (double)non_zero_px / (diff_mat.cols * diff_mat.rows)) * 100.0;
+    std::cout << "Similarity " << s << "%" << std::endl;
 }
 
 
-cv::Mat box_filter_kernel(int size)
+cv::Mat box_filter_kernel(cv::Size2i kernel_size)
 {
-    cv::Mat kernel = cv::Mat::ones(size, size, CV_32F);
-    kernel /= (float)(size * size);
+    cv::Mat kernel = cv::Mat::ones(kernel_size, CV_32F);
+    kernel /= (float)(kernel_size.width * kernel_size.height);
 
     return kernel;
 }
 
 
-cv::Mat gauss_filter_kernel(int size)
+cv::Mat gauss_filter_kernel(cv::Size2i size, double sigma)
 {
-    cv::Mat kernel(size, size, CV_32F);
+    cv::Mat kernel(size, CV_32F);
 
-    for (int x = 0; x < size; x++) {
-        for (int y = 0; y < size; y++) {
-            double sigma = 0.3 * ((size - 1) * 0.5 - 1.0) + 0.8;
-            
+    for (int i = 0; i < size.width; i++) {
+        for (int j = 0; j < size.height; j++) {
+            int center = size.width / 2;
+            int x = i - center;
+            int y = i - center;
+
+            double value = std::exp(-(x * x + y * y) / (2.0 * sigma));
+            kernel.at<double>(i, j) = value;
         }
     }
 }
@@ -42,35 +49,56 @@ cv::Mat gauss_filter_kernel(int size)
 
 int main()
 {
-    std::str task = "box_filter";
-    cv::Mat src = "image.jpg";
+    cv::Mat src = cv::imread("250px-Lenna.png");
+    cv::cvtColor(src, src, cv::COLOR_BGR2GRAY);
+    cv::Size2i kernel_size(3, 3);
+
+    cv::Mat box_dst, blur_dst;
+/*
+    double tick_start = cv::getTickCount();
+    cv::filter2D(src, box_dst, -1, box_filter_kernel(kernel_size), cv::Point(-1, -1), 0.0, cv::BORDER_CONSTANT);
+    double time_end = (double)(cv::getTickCount() - tick_start) / cv::getTickFrequency();
+    std::cout << "Custom box: " << time_end << std::endl;
+
+    tick_start = cv::getTickCount();
+    cv::blur(src, blur_dst, kernel_size);
+    time_end = (cv::getTickCount() - tick_start) / cv::getTickFrequency();
+    std::cout << "Blur: "<< time_end << std::endl;
+
+    cv::imshow("Source image", src);
+    cv::imshow("Filtered image", box_dst);
+    cv::imshow("Filtered with blur image", blur_dst);
+
+    calc_diff(box_dst, blur_dst);
+*/
+    cv::Mat gauss_dst, diff_dst, log_diff_dst;
+
+    double sigma = 1.0;
+
+    double tick_start = cv::getTickCount();
+    cv::blur(src, blur_dst, kernel_size);
+    double time_box = (cv::getTickCount() - tick_start) / cv::getTickFrequency();
+
+    tick_start = cv::getTickCount();
+    cv::GaussianBlur(src, gauss_dst, kernel_size, sigma);
+    double time_gauss = (cv::getTickCount() - tick_start) / cv::getTickFrequency();
+
+    std::cout << "Box filter time: " << time_box << " s" << std::endl;
+    std::cout << "Gauss filter time: " << time_gauss << " s" << std::endl;
+
+    calc_diff(blur_dst, gauss_dst);
     
-    long tick_start = cv::getTickCount();
+    diff_dst.convertTo(diff_dst, CV_32F);
+    
+    log_diff_dst.convertTo(log_diff_dst, CV_32F);
+    cv::intensity_transform::logTransform(diff_dst, log_diff_dst);
 
-    switch (task)
-    {
-    case "box_filter":
-        cv::Mat box_dst, blur_dst;
+    cv::imshow("Original", src);
+    cv::imshow("Box Filter", blur_dst);
+    cv::imshow("Gauss Filter", gauss_dst);
+    cv::imshow("Difference", diff_dst);
+    cv::imshow("Log Difference", log_diff_dst);
 
-        long tick_start = cv::getTickCount();
-        cv::filter2D(src, box_dst, box_filter_kernel(3));
-        long time_end = (cv::getTickCount - time_start) / cv::getTickFrequency;
-        std::cout << time_end;
-
-        long tick_start = cv::getTickCount();
-        cv::Mat blur = cv::blur(src, blur_dst, size);
-        long time_end = (cv::getTickCount - time_start) / cv::getTickFrequency;
-        std::cout << time_end;
-
-        cv::imshow("Source image", src);
-        cv::imshow("Filtered image", box_dst);
-        cv::imshow("Filtered with blur image", blur);
-
-        calc_diff(box_dst, blur_dst);
-        cv::waitKey();
-        cv::destroyAllWindows();
-
-    }
 
     cv::waitKey();
     cv::destroyAllWindows();
