3baafa7081772e299d96a22ca845301b929e8b8b
greisersem <grejser.se@edu.spbstu.ru>
2025-12-14 21:42:32 +0300

    Lab_3

---
+++ lab_3/cvDirectory.h
@@ -0,0 +1,10 @@
+namespace cv
+{
+	class Directory
+	{
+	public:
+		static std::vector<std::string> GetListFiles(const std::string& path, const std::string & exten = "*", bool addPath = true);
+		static std::vector<std::string> GetListFilesR(const std::string& path, const std::string & exten = "*", bool addPath = true);
+		static std::vector<std::string> GetListFolders(const std::string& path, const std::string & exten = "*", bool addPath = true);
+	};
+}
