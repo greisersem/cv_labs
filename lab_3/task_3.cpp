20dac2c84a8261015059fe98a0efeca2bc311109
greisersem <grejser.se@edu.spbstu.ru>
2025-12-16 20:08:14 +0000

    task_3

--- lab_3/task_3.cpp
+++ lab_3/task_3.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <vector>
+#include <math.h>
 #include <opencv2/opencv.hpp>
 #include <opencv2/highgui/highgui.hpp>
 #include <opencv2/imgproc/imgproc.hpp>
@@ -7,7 +8,7 @@
 
 void draw_rectangle(cv::Mat img, cv::Point center, cv::Scalar color)
 {
-    int size = 40;
+    int size = 60;
     cv::Rect rect(
         center.x - size / 2,
         center.y - size / 2,
@@ -18,12 +19,12 @@ void draw_rectangle(cv::Mat img, cv::Point center, cv::Scalar color)
 }
 
 
-cv::Mat find_nearest(cv::Mat img, cv::Point bulb_coords, std::string team)
+cv::Mat find_nearest(cv::Mat img, cv::Point bulb_coords, std::string team, cv::Mat res)
 {
     cv::Mat src_hsv;
     cv::cvtColor(img, src_hsv, cv::COLOR_BGR2HSV);
 
-    cv::Scalar rect_color;
+    cv::Scalar figures_color;
     if (team == "red") {
         cv::Mat temp_1, temp_2;
         cv::inRange(
@@ -39,7 +40,16 @@ cv::Mat find_nearest(cv::Mat img, cv::Point bulb_coords, std::string team)
             temp_2
         );
         src_hsv = temp_1 | temp_2;
-        rect_color = cv::Scalar(0, 0, 255);
+
+        cv::circle(
+            src_hsv, 
+            cv::Point(bulb_coords.x - 5, bulb_coords.y - 40),
+            35, 
+            cv::Scalar(0,0,0),
+            -1
+        );
+
+        figures_color = cv::Scalar(0, 0, 255);
     } else if (team == "blue")
     {
         cv::inRange(
@@ -48,7 +58,7 @@ cv::Mat find_nearest(cv::Mat img, cv::Point bulb_coords, std::string team)
             cv::Scalar(105, 255, 255),
             src_hsv
         );
-        rect_color = cv::Scalar(255, 0, 0);
+        figures_color = cv::Scalar(255, 0, 0);
     } else if (team == "green")
     {
         cv::inRange(
@@ -57,7 +67,7 @@ cv::Mat find_nearest(cv::Mat img, cv::Point bulb_coords, std::string team)
             cv::Scalar(80, 255, 255),
             src_hsv
         );
-        rect_color = cv::Scalar(0, 255, 0);
+        figures_color = cv::Scalar(0, 255, 0);
     }
 
     std::vector <std::vector <cv::Point>> contours;
@@ -71,16 +81,26 @@ cv::Mat find_nearest(cv::Mat img, cv::Point bulb_coords, std::string team)
         }
     }
 
+    double min_dist = 1e6;
+    cv::Point best_center;
+
     for (int i = 0; i < contours.size(); i++) {
         if (contours[i].size() > largest_contour.size() * 0.3) {
             cv::Moments moments = cv::moments(contours[i]);
             int center_x = (moments.m10 / moments.m00);
             int center_y = (moments.m01 / moments.m00);
-            draw_rectangle(img, cv::Point(center_x, center_y), rect_color);
+            draw_rectangle(res, cv::Point(center_x, center_y), figures_color);
+            
+            double distance_to_bulb = 
+                sqrt(pow(center_x - bulb_coords.x, 2) + pow(center_y - bulb_coords.y, 2));
+            if (distance_to_bulb < min_dist) {
+                min_dist = distance_to_bulb;
+                best_center = cv::Point(center_x, center_y);
+            }
         }
     }
 
-    return src_hsv;
+    cv::line(res, best_center, bulb_coords, figures_color, 2);
 }
 
 
@@ -119,20 +139,21 @@ cv::Point find_bulb(cv::Mat img)
 
 void robots_detection(cv::Mat img)
 {
+    cv::Mat res = img.clone();
     cv::Point bulb_coords = find_bulb(img);
-    draw_rectangle(img, bulb_coords, cv::Scalar(0, 0, 0));
+    draw_rectangle(res, bulb_coords, cv::Scalar(0, 0, 0));
 
     int red_team_hue = 10;
     int blue_team_hue = 95;
     int green_team_hue = 70;
 
-    cv::Mat red = find_nearest(img, bulb_coords, "red");
+    cv::Mat red = find_nearest(img, bulb_coords, "red", res);
     cv::imshow("red", red);
-    cv::Mat blue = find_nearest(img, bulb_coords, "blue");
+    cv::Mat blue = find_nearest(img, bulb_coords, "blue", res);
     cv::imshow("blue", blue);
-    cv::Mat green = find_nearest(img, bulb_coords, "green");
+    cv::Mat green = find_nearest(img, bulb_coords, "green", res);
     cv::imshow("green", green);
-    cv::imshow("img", img);
+    cv::imshow("img", res);
 }
 
 
