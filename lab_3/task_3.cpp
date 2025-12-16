a84ac867c3921dd9899bad90405f5abe4bfbc428
greisersem <grejser.se@edu.spbstu.ru>
2025-12-16 15:50:49 +0000

    task_3

--- lab_3/task_3.cpp
+++ lab_3/task_3.cpp
@@ -5,22 +5,36 @@
 #include <opencv2/imgproc/imgproc.hpp>
 
 
+void draw_rectangle(cv::Mat img, cv::Point center, cv::Scalar color)
+{
+    int size = 40;
+    cv::Rect rect(
+        center.x - size / 2,
+        center.y - size / 2,
+        size,
+        size
+    );
+    cv::rectangle(img, rect, color, 2);
+}
+
+
 cv::Mat find_nearest(cv::Mat img, cv::Point bulb_coords, int robot_team_hue)
 {
-    cv::Mat temp = img.clone();
+    cv::Mat src_hsv;
+    cv::cvtColor(img, src_hsv, cv::COLOR_BGR2HSV);
 
     int h_min = std::max(0, robot_team_hue - 10);
     int h_max = std::min(179, robot_team_hue + 10);
 
     cv::inRange(
-        temp,
-        cv::Scalar(h_min, 0, 20),
+        src_hsv,
+        cv::Scalar(h_min, 120, 120),
         cv::Scalar(h_max, 255, 255),
-        temp
+        src_hsv
     );
 
     std::vector <std::vector <cv::Point>> contours;
-    cv::findContours(temp, contours, cv::RETR_EXTERNAL, cv::CHAIN_APPROX_NONE);
+    cv::findContours(src_hsv, contours, cv::RETR_EXTERNAL, cv::CHAIN_APPROX_NONE);
 
     std::vector <cv::Point> largest_contour = contours[0];
 
@@ -31,30 +45,27 @@ cv::Mat find_nearest(cv::Mat img, cv::Point bulb_coords, int robot_team_hue)
     }
 
     for (int i = 0; i < contours.size(); i++) {
-        if (contours[i].size() > largest_contour.size() * 0.8) {
+        if (contours[i].size() > largest_contour.size() * 0.1) {
             cv::Moments moments = cv::moments(contours[i]);
             int center_x = (moments.m10 / moments.m00);
             int center_y = (moments.m01 / moments.m00);
-
-            cv::Rect rect(center_x - 20, center_y - 20, 40, 40);
-            cv::rectangle(img, rect, cv::Scalar(0,0,0));
-            cv::imshow("gg", img);
-            cv::waitKey();
+            draw_rectangle(img, cv::Point(center_x, center_y), cv::Scalar(0,0,0));
         }
     }
 
-    return temp;
+    return src_hsv;
 }
 
 
 cv::Point find_bulb(cv::Mat img)
 {
     cv::Mat bulb;
+    cv::cvtColor(img, bulb, cv::COLOR_BGR2HSV);
     cv::Scalar bulb_min(0, 0, 250);
-    cv::Scalar bulb_max(150, 30, 255);
+    cv::Scalar bulb_max(150, 10, 255);
 
     cv::inRange(
-        img, 
+        bulb, 
         bulb_min,
         bulb_max, 
         bulb
@@ -81,29 +92,20 @@ cv::Point find_bulb(cv::Mat img)
 
 void robots_detection(cv::Mat img)
 {
-    cv::Mat src_hsv(img.size(), CV_8UC1);
-    cv::cvtColor(img, src_hsv, cv::COLOR_BGR2HSV);
+    cv::Point bulb_coords = find_bulb(img);
+    draw_rectangle(img, bulb_coords, cv::Scalar(0, 0, 0));
 
-    cv::Point bulb_coords = find_bulb(src_hsv);
-    int size = 40;
-    cv::Rect rect(
-        bulb_coords.x - size / 2,
-        bulb_coords.y - size / 2,
-        size,
-        size
-    );
-    cv::rectangle(img, rect, cv::Scalar(0, 0, 0));
-
-    int red_team_hue = 172;
+    int red_team_hue = 10;
     int blue_team_hue = 95;
     int green_team_hue = 70;
 
-    cv::Mat red = find_nearest(src_hsv, bulb_coords, red_team_hue);
+    cv::Mat red = find_nearest(img, bulb_coords, red_team_hue);
     cv::imshow("red", red);
-    cv::Mat blue = find_nearest(src_hsv, bulb_coords, blue_team_hue);
+    cv::Mat blue = find_nearest(img, bulb_coords, blue_team_hue);
     cv::imshow("blue", blue);
-    cv::Mat green = find_nearest(src_hsv, bulb_coords, green_team_hue);
+    cv::Mat green = find_nearest(img, bulb_coords, green_team_hue);
     cv::imshow("green", green);
+    cv::imshow("img", img);
     cv::waitKey();
 }
 
